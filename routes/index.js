da8aa2f670d2271d94bd560a06126e39bd285b5a
bluephlavio <flavio.grandin@gmail.com>
2018-05-19 13:10:46 +0200

    routes and controllers implemented

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+// const _ = require('underscore');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const openweathermap = require('../openweathermap');
+const flickr = require('../flickr');
+
+const mongoose = require('mongoose');
+const db = require('../db');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+  db.fetchCurrent(data => {
+    console.log(data);
+    res.render('index', data);
+  });
 });
+
+router.get('/openweathermap', (req, res) => {
+  openweathermap.query(weather => {
+    res.json(weather);
+  })
+});
+
+router.get('/flickr', (req, res) => {
+  flickr.query({
+    name: req.query.name,
+  }, photos => {
+    if (req.query.display) {
+      res.setHeader('Content-Type', 'text/html');
+      res.end(_.map(photos, photo => {
+        return `<img src="${photo}" />`;
+      }).join('\n'));
+    } else {
+      res.json(photos);
+    }
+  });
+});
+
 
 module.exports = router;
