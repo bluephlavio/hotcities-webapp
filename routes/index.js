75e384304048a9bb3c315821095fca1f460fbff6
bluephlavio <flavio.grandin@gmail.com>
2018-05-21 22:29:20 +0200

    api routes defined

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,30 @@ const db = require('../db');
 const router = express.Router();
 
 
+router.get('/api/cities/', (req, res) => {
+  db.City.find({}, (error, cities) => {
+    res.json(cities);
+  });
+});
+
+router.get('/api/cities/:name', (req, res) => {
+  db.City.findOne({ name: req.params.name }).exec( (error, city) => {
+    res.json(city);
+  });
+});
+
+router.get('/api/records/', (req, res) => {
+  db.Record.find({}).sort({ timestamp: -1 }).exec( (error, records) => {
+    res.json(records);
+  });
+});
+
+router.get('/api/records/current', (req, res) => {
+  db.Record.findOne({}).sort({ timestamp: -1 }).exec( (error, current) => {
+    res.json(current);
+  });
+});
+
 router.get('/', (req, res) => {
   db.fetchCurrent(data => {
     if (data) {
