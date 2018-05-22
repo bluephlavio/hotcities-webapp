7cfbd086e441cdacfc385ce2723abc86985a8452
bluephlavio <flavio.grandin@gmail.com>
2018-05-22 20:53:05 +0200

    openweathermap service cleaned

--- openweathermap.js
+++ openweathermap.js
@@ -1,9 +1,4 @@
 const buildUrl = require('build-url');
-const request = require('request');
-const async = require('async');
-const _ = require('underscore');
-
-const db = require('./db');
 
 
 function queryUrl(zoom) {
@@ -16,31 +11,13 @@ function queryUrl(zoom) {
     });
 }
 
-function fetchWeatherInfo(data, city) {
-  let weather = _.find(data.list, entry => {
-    return entry.name == city.name;
-  });
-  return {
-    city: city,
-    weather: weather
-  }
-}
-
 
 module.exports = {
 
-  query: function(callback) {
-    request(queryUrl(12), (error, responde, body) => {
+  query: callback => {
+    request(queryUrl(zoom), (error, responde, body) => {
       let data = JSON.parse(body);
-      let availables = _.map(data.list, entry => {
-        return entry.name;
-      });
-      db.City.find({ name: {$in: availables}}, (err, cities) => {
-        let weather = _.map(cities, city => {
-          return fetchWeatherInfo(data, city);
-        });
-        callback(weather);
-      });
+      callback(data);
     });
   }
 
