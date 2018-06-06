57849c2c506c95da110ec9c94a42a55b4b42e841
bluephlavio <flavio.grandin@gmail.com>
2018-06-06 21:48:03 +0200

    promise based new services implementation ported on external-api

--- routes/external-api.js
+++ routes/external-api.js
@@ -6,17 +6,19 @@ const _ = require('underscore');
 const openweathermap = require('../openweathermap');
 const flickr = require('../flickr');
 
-router.get('/openweathermap', (req, res) => {
-  openweathermap.query(data => {
-    res.json(data);
-  });
+router.get('/openweathermap/', (req, res) => {
+  openweathermap.query()
+    .then(data => {
+      res.json(data);
+    });
 });
 
-router.get('/flickr', (req, res) => {
+router.get('/flickr/', (req, res) => {
   let params = req.query ? req.query : {};
-  flickr.query(params, data => {
-    res.json(data);
-  });
+  flickr.query(params)
+		.then(data => {
+    	res.json(data);
+		});
 });
 
 module.exports = router;
