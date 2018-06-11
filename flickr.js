cac57a05a9ca459742b16aa64ab8124af7398cd7
bluephlavio <flavio.grandin@gmail.com>
2018-06-11 18:38:31 +0200

    flickr service reimplemented using flickr-sdk

--- flickr.js
+++ flickr.js
@@ -1,37 +1,6 @@
 require('dotenv')
   .config();
 
-const request = require('request');
-const buildUrl = require('build-url');
-const _ = require('underscore');
+const Flickr = require('flickr-sdk');
 
-function queryUrl(params) {
-  return buildUrl('https://api.flickr.com/', {
-    path: 'services/rest/',
-    queryParams: _.defaults(params, {
-      method: 'flickr.photos.search',
-      api_key: process.env.FLICKR_KEY,
-      format: 'json',
-      nojsoncallback: 1,
-      sort: 'relevance',
-      accuracy: 11
-    })
-  });
-}
-
-module.exports = {
-
-  query: params => {
-    return new Promise((resolve, reject) => {
-      request.get(queryUrl(params), (err, res, body) => {
-        let data = JSON.parse(body);
-        resolve(data);
-      });
-    });
-  },
-
-  buildPhotoUrl: (farm, server, id, secret) => {
-    return `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_b.jpg`
-  }
-
-}
+module.exports = new Flickr(process.env.FLICKR_KEY);
