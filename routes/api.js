46a10373e852f94dd0e5f116f7e5ccc17dd111da
bluephlavio <flavio.grandin@gmail.com>
2018-06-11 18:35:50 +0200

    api routes completely rewritten using mongodb aggragation and implementing geojson output

--- routes/api.js
+++ routes/api.js
@@ -1,70 +1,132 @@
+require('dotenv')
+  .config();
+
 const express = require('express');
 const router = express.Router();
 
 const _ = require('underscore');
+const geojson = require('geojson')
 
 const db = require('../db');
+db.open();
 
-router.get('/cities/', (req, res) => {
-  let criteria = req.query.all ? {} : { records: { $not: { $size: 0 } } };
-  db.City.find(criteria)
-    .populate('records')
-    .then(cities => {
-      let data = _.map(cities, city => {
-        return city.toObject();
-      });
-      if (req.query.stats) {
-        data = _.map(data, city => {
-          let recordsCount = city.records.length;
-          let recordTemp = recordsCount ? _.max(_.map(city.records, record => {
-            return record.temp;
-          })) : null;
-          city.stats = {
-            recordsCount,
-            recordTemp
-          }
-          return city;
-        });
-      }
-      if (!req.query.records) {
-        data = _.map(data, city => {
-          return _.omit(city, 'records');
-        });
-      }
+
+router.get('/cities', (req, res) => {
+	let agg = db.City.aggregate()
+		.lookup({
+			from: 'records',
+			localField: 'geonameid',
+			foreignField: 'geonameid',
+			as: 'records'
+		})
+		.addFields({
+			maxTemp: { $max: '$records.temp'},
+			records: { $size: '$records' }
+		})
+		.project({_id: 0})
+		.sort(req.query.sort ? req.query.sort.replace(',', ' ') : 'name');
+	if (req.query.filter == 'records-only') {
+		agg = agg.match({records: {$gt: 0}});
+	}
+	if (req.query.limit) {
+		let num = Number(req.query.limit);
+		if (num > 0) {
+			agg = agg.limit(num);
+		}
+	}
+	agg.exec().then(data => {
+			if (req.query.geojson == 'true') {
+				data = geojson.parse(data, {Point: ['lat', 'lng']});
+			}
+			res.json(data);
+		});
+});
+
+router.get('/cities/:id', (req, res) => {
+	db.City.aggregate()
+		.match({
+			geonameid: Number(req.params.id)
+		})
+		.lookup({
+			from: 'records',
+			localField: 'geonameid',
+			foreignField: 'geonameid',
+			as: 'records'
+		})
+		.addFields({
+			maxTemp: { $max: '$records.temp'},
+			records: { $size: '$records' }
+		})
+		.project({_id: 0})
+		.exec()
+		.then(data => {
+			data = data[0];
+			if (req.query.geojson == 'true') {
+				data = geojson.parse(data, {Point: ['lat', 'lng']});
+			}
+			res.json(data);
+		})
+});
+
+router.get('/records', (req, res) => {
+	agg = db.Record.aggregate()
+		.lookup({
+			from: 'cities',
+			localField: 'geonameid',
+			foreignField: 'geonameid',
+			as: 'city'
+		})
+		.unwind('$city')
+		.replaceRoot({
+			$mergeObjects: ['$$ROOT', '$city']
+		})
+		.project({
+			_id: 0,
+			__v: 0,
+			city: 0
+		})
+		.sort(req.query.sort ? req.query.sort.replace(',', ' ') : '-timestamp');
+	if (req.query.limit) {
+		let num = Number(req.query.limit);
+		if (num > 0) {
+			agg = agg.limit(num);
+		}
+	}
+	agg.exec().then(data => {
+			if (req.query.geojson == 'true') {
+				data = geojson.parse(data, {Point: ['lat', 'lng']});
+			}
+			res.json(data);
+		});
+});
+
+router.get('/records/current', (req, res) => {
+	db.Record.aggregate()
+		.sort('-timestamp')
+		.limit(1)
+		.lookup({
+			from: 'cities',
+			localField: 'geonameid',
+			foreignField: 'geonameid',
+			as: 'city'
+		})
+		.unwind('$city')
+		.replaceRoot({
+			$mergeObjects: ['$$ROOT', '$city']
+		})
+		.project({
+			_id: 0,
+			__v: 0,
+			city: 0
+		})
+		.exec()
+    .then(data => {
+			if (req.query.geojson == 'true') {
+				data = geojson.parse(data, {Point: ['lat', 'lng']});
+			}
       res.json(data);
     });
 });
 
-router.get('/cities/:name', (req, res) => {
-  db.City.findOne({
-      name: req.params.name
-    })
-    .populate('records')
-    .exec((error, city) => {
-      res.json(city);
-    });
-});
-
-router.get('/records/', (req, res) => {
-  db.Record.find({})
-    .sort({
-      timestamp: -1
-    })
-    .populate('city')
-    .exec((error, records) => {
-      res.json(records);
-    });
-});
-
-router.get('/records/current', (req, res) => {
-  db.Record.findOne({})
-    .sort({
-      timestamp: -1
-    })
-    .populate('city')
-    .exec((error, current) => {
-      res.json(current);
-    });
-});
 
 module.exports = router;
--- routes/external-api.js
+++ routes/external-api.js
@@ -6,18 +6,24 @@ const _ = require('underscore');
 const openweathermap = require('../openweathermap');
 const flickr = require('../flickr');
 
-router.get('/openweathermap/', (req, res) => {
+router.get('/openweathermap', (req, res) => {
   openweathermap.query()
     .then(data => {
       res.json(data);
-    });
+    })
+		.catch(error => {
+			console.log(error);
+		});
 });
 
-router.get('/flickr/', (req, res) => {
-  let params = req.query ? req.query : {};
-  flickr.query(params)
-		.then(data => {
-    	res.json(data);
+router.get('/flickr', (req, res) => {
+  flickr.photos.search(req.query)
+		.ok(res => res.status < 500)
+		.then(response => {
+    	res.json(response.body);
+		})
+		.catch(error => {
+			console.log(error);
 		});
 });
 
