bfa4ca5adae230116bf9437a73203dbad9738e4d
bluephlavio <flavio.grandin@gmail.com>
2018-06-14 10:37:35 +0200

    logging cleaned on datafetcher script

--- bin/datafetcher.js
+++ bin/datafetcher.js
@@ -1,5 +1,5 @@
 require('dotenv')
-  .config();
+	.config();
 
 const _ = require('underscore');
 const randomItem = require('random-item');
@@ -12,60 +12,60 @@ const db = require('../db');
 
 function findRecord() {
 	return Promise.all([
-		openweathermap.query(),
-		db.City.find({population: {$gt: 500000} })
-	])
-	.then(results => {
-		let weather = results[0];
-		let cities = results[1];
-	  let geonameids = _.map(cities, city => {
-	    return city.geonameid;
-	  });
-	  let availables = _.filter(weather.list, entry => {
-	    return _.contains(geonameids, entry.id);
-	  });
-	  let temperatures = _.map(availables, entry => {
-	    return entry.main.temp;
-	  });
-	  let maxTemp = _.max(temperatures);
-	  let candidates = _.filter(availables, entry => {
-	    return entry.main.temp == maxTemp;
-	  });
-	  let record = randomItem(candidates);
-		return Promise.all([
-			record,
-			db.City.findOne({geonameid: record.id})
-		]);
-	});
+			openweathermap.query(),
+			db.City.find({ population: { $gt: 500000 } })
+		])
+		.then(results => {
+			let weather = results[0];
+			let cities = results[1];
+			let geonameids = _.map(cities, city => {
+				return city.geonameid;
+			});
+			let availables = _.filter(weather.list, entry => {
+				return _.contains(geonameids, entry.id);
+			});
+			let temperatures = _.map(availables, entry => {
+				return entry.main.temp;
+			});
+			let maxTemp = _.max(temperatures);
+			let candidates = _.filter(availables, entry => {
+				return entry.main.temp == maxTemp;
+			});
+			let record = randomItem(candidates);
+			return Promise.all([
+				record,
+				db.City.findOne({ geonameid: record.id })
+			]);
+		});
 }
 
 function findView(city) {
 	return flickr.photos.search({
-		text: city.name,
-		format: 'json',
-		nojsoncallback: 1,
-		sort: 'relevance'
-	})
-	.then(async res => {
-		let photos = res.body.photos.photo;
-		let candidates = photos.slice(0, 10);
-		for (candidate of candidates) {
-			let sizes = await flickr.photos.getSizes({
-				photo_id: candidate.id
-			})
-			.then(res => {
-				return res.body.sizes.size;
-			});
-			let size = _.find(sizes, size => {
-				return Math.abs(size.width - 1024) < 100;
-			});
-			if (size) {
-				return size.source;
-				break;
+			text: city.name,
+			format: 'json',
+			nojsoncallback: 1,
+			sort: 'relevance'
+		})
+		.then(async res => {
+			let photos = res.body.photos.photo;
+			let candidates = photos.slice(0, 10);
+			for (candidate of candidates) {
+				let sizes = await flickr.photos.getSizes({
+						photo_id: candidate.id
+					})
+					.then(res => {
+						return res.body.sizes.size;
+					});
+				let size = _.find(sizes, size => {
+					return Math.abs(size.width - 1024) < 100;
+				});
+				if (size) {
+					return size.source;
+					break;
+				}
 			}
-		}
-		return null;
-	});
+			return null;
+		});
 }
 
 
@@ -85,7 +85,6 @@ db.open()
 		});
 	})
 	.then(record => {
-		console.log(record);
 		db.close()
 	})
 	.catch(error => {
