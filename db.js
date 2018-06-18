c4c583bbe37d2f6df2cc3339eaecaa2eac73c51a
bluephlavio <flavio.grandin@gmail.com>
2018-06-18 17:59:06 +0200

    view model added

--- db.js
+++ db.js
@@ -2,22 +2,24 @@ const mongoose = require('mongoose');
 
 const City = require('./models/city');
 const Record = require('./models/record');
+const View = require('./models/view');
 
 mongoose.Promise = global.Promise;
 
 module.exports.connection = mongoose.connection;
 module.exports.open = () => {
-  return mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-      console.log('Connected to the database.');
-    });
+	return mongoose.connect(process.env.MONGODB_URI)
+		.then(() => {
+			console.log('Connected to the database.');
+		});
 };
 module.exports.close = () => {
-  return mongoose.connection.close()
-    .then(() => {
-      console.log('Disconnected from the database.');
-    });
+	return mongoose.connection.close()
+		.then(() => {
+			console.log('Disconnected from the database.');
+		});
 }
 
 module.exports.City = City;
 module.exports.Record = Record;
+module.exports.View = View;
