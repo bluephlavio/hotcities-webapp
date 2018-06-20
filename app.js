32246b1f0a84c15c2594f0bb04de84b882365784
bluephlavio <flavio.grandin@gmail.com>
2018-06-20 07:41:49 +0200

    garbage removed, project cleaned

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'client', 'build', 'favicon.ico')));
 app.use(logger('dev'));
 
 app.use('/api', require('./routes/api'));
