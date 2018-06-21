40e5ca27aec62ffd73e427379ba6d770ff019041
bluephlavio <flavio.grandin@gmail.com>
2018-06-21 10:28:05 +0200

    google analytics added

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import registerServiceWorker from './registerServiceWorker';
 import React from 'react';
+import ReactGA from 'react-ga';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
@@ -9,8 +10,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome5/css/fontawesome-all.min.css';
 import './index.css';
 
+ReactGA.initialize('UA-55404180-2');
+
+function fireTracking() {
+	ReactGA.pageview(window.location.hash);
+}
+
 ReactDOM.render(
-	<BrowserRouter>
+	<BrowserRouter onUpdate={fireTracking} >
 		<App />
 	</BrowserRouter>,
 	document.getElementById('root'));
