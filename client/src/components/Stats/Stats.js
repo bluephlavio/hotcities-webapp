bc1272d7a139cf93b95bb20a71d4da1c927a4939
bluephlavio <flavio.grandin@gmail.com>
2018-06-21 17:52:56 +0200

    default ranking method change to max temperature

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -22,7 +22,7 @@ class Ranking extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			rankFn: this.rankByRecordFrac,
+			rankFn: this.rankByRecordTemp,
 			expanded: true
 		};
 	}
