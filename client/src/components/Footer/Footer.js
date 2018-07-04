057452354bf151584c49e95ad4d9bb92b19c328c
bluephlavio <flavio.grandin@gmail.com>
2018-07-04 10:56:45 +0200

    instagram and facebook social buttons added.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,32 +4,46 @@ import './Footer.css';
 const Social = (props) => {
 	return (
 		<div className="footer-social">
-			<ul className="nav justify-content-center">
-				<li className="nav-item">
-					<button className="btn btn-social-icon">
-						<a href="https://twitter.com/intent/user?screen_name=hotcitiesworld" target="_blank" rel="noopener noreferrer">
-							<span className="fab fa-twitter"></span>
-						</a>
-					</button>
-				</li>
-				<li className="nav-item">
-					<button className="btn btn-social-icon">
-						<a href="https://github.com/bluephlavio/hotcities" target="_blank" rel="noopener noreferrer">
-							<span className="fab fa-github"></span>
-						</a>
-					</button>
-				</li>
-			</ul>
-		</div>
+            <ul className="nav justify-content-center">
+                <li className="nav-item">
+                    <button className="btn btn-social-icon">
+                        <a href="https://twitter.com/intent/user?screen_name=hotcitiesworld" target="_blank" rel="noopener noreferrer">
+                            <span className="fab fa-twitter"></span>
+                        </a>
+                    </button>
+                </li>
+                <li className="nav-item">
+                    <button className="btn btn-social-icon">
+                        <a href="https://www.instagram.com/hotcitiesworld/" target="_blank" rel="noopener noreferrer">
+                            <span className="fab fa-instagram"></span>
+                        </a>
+                    </button>
+                </li>
+                <li className="nav-item">
+                    <button className="btn btn-social-icon">
+                        <a href="https://www.facebook.com/hotcitiesworld" target="_blank" rel="noopener noreferrer">
+                            <span className="fab fa-facebook"></span>
+                        </a>
+                    </button>
+                </li>
+                <li className="nav-item">
+                    <button className="btn btn-social-icon">
+                        <a href="https://github.com/bluephlavio/hotcities" target="_blank" rel="noopener noreferrer">
+                            <span className="fab fa-github"></span>
+                        </a>
+                    </button>
+                </li>
+            </ul>
+        </div>
 	);
 }
 
 const Footer = (props) => {
 	return (
 		<footer className="footer">
-			<hr />
-			<Social />
-		</footer>
+            <hr />
+            <Social />
+        </footer>
 	);
 }
 
