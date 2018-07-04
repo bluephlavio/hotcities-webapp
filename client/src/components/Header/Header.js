bc0488dd65802eed0ad918c89a3610f503c169bc
bluephlavio <flavio.grandin@gmail.com>
2018-07-04 10:50:13 +0200

    seo meta tags optimized

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,21 +5,21 @@ import './Header.css';
 const Menu = (props) => {
 	return (
 		<div className="header-menu">
-			<ul className="navbar-nav">
-				<li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-					<Link className="nav-link" to="/">Live</Link>
-				</li>
-				<li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-					<Link className="nav-link" to="/stats">Stats</Link>
-				</li>
-				<li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-					<Link className="nav-link" to="/apidocs">API</Link>
-				</li>
-				<li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-					<Link className="nav-link" to="/about">About</Link>
-				</li>
-			</ul>
-		</div>
+            <ul className="navbar-nav">
+                <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                    <Link className="nav-link" to="/">Live</Link>
+                </li>
+                <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                    <Link className="nav-link" to="/stats">Stats</Link>
+                </li>
+                <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                    <Link className="nav-link" to="/apidocs">API</Link>
+                </li>
+                <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                    <Link className="nav-link" to="/about">About</Link>
+                </li>
+            </ul>
+        </div>
 	);
 }
 
@@ -27,35 +27,35 @@ class Header extends Component {
 	render() {
 		return (
 			<header className="header">
-				<nav className="navbar navbar-expand-md">
-					<div className="navbar-header">
-						<a href="/" className="navbar-brand">
-							<h1 className="title">
-								HOT CITIES
-							</h1>
-							<h2 className="motto">world's hottest city, now</h2>
-						</a>
-					</div>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-toggle="collapse"
-						data-target="#nav-id"
-						aria-controls="#navId"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="fa fa-bars"></span>
-					</button>
-					<div
-						className="collapse navbar-collapse justify-content-end"
-						id="nav-id"
-					>
-						<Menu />
-					</div>
-				</nav>
-				<hr />
-			</header>
+                <nav className="navbar navbar-expand-md">
+                    <div className="navbar-header">
+                        <a href="/" className="navbar-brand">
+                            <h1 className="title">
+                                HOT CITIES
+                            </h1>
+                            <h2 className="motto">world's hottest city, now.</h2>
+                        </a>
+                    </div>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#nav-id"
+                        aria-controls="#navId"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="fa fa-bars"></span>
+                    </button>
+                    <div
+                        className="collapse navbar-collapse justify-content-end"
+                        id="nav-id"
+                    >
+                        <Menu />
+                    </div>
+                </nav>
+                <hr />
+            </header>
 		);
 	}
 }
