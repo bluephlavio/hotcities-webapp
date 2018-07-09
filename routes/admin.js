57bfc0392dd94c10540aa5cb81b9c7f48ad44a7e
bluephlavio <flavio.grandin@gmail.com>
2018-07-09 21:31:45 +0200

    admin routes set up with authentication logic

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+const db = require('../db');
+const geoname = require('../geoname');
+const openweathermap = require('../openweathermap');
+const flickr = require('../flickr');
+
 router.get('/login', (req, res) => {
 	if (req.session.admin) {
 		res.redirect('/admin');
@@ -17,9 +22,9 @@ router.post('/login', (req, res) => {
 	} else {
 		res.redirect('/admin/login');
 	}
-})
+});
 
-router.use((req, res, next) => {
+router.use('/', (req, res, next) => {
 	if (req.session.admin) {
 		next();
 	} else {
@@ -27,6 +32,16 @@ router.use((req, res, next) => {
 	}
 });
 
-router.use(express.static(path.join(__dirname, '..', 'admin', 'public')));
+router.get('/views/fetch', async (req, res) => {
+	let city = await geoname.findCityByName(req.query.city);
+	console.log(city);
+	let views = await flickr.fetchViewsAndSave(city, {});
+	res.redirect('/admin/views');
+});
+
+router.use('/', express.static(path.join(__dirname, '..', 'admin', 'build')));
+router.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '..', 'admin', 'build', 'index.html'));
+});
 
 module.exports = router;
