4b2c78824920563a8350d6c8e06d41e32d23f420
bluephlavio <flavio.grandin@gmail.com>
2018-07-09 21:34:07 +0200

    minor client refactoring

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,19 +1,17 @@
-import React, {Component} from "react";
+import React from "react";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Header />
-        <Main />
-				<Footer />
-      </div>
-    );
-  }
+const App = (props) => {
+	return (
+		<div className="app">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+	);
 }
 
 export default App;
