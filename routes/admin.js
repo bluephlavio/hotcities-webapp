4ba18a4460061b911b2a44c99b1bb899f90d6288
bluephlavio <flavio.grandin@gmail.com>
2018-07-09 08:12:44 +0200

    admin routes added

---
+++ routes/admin.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+	if (req.session.admin) {
+		res.redirect('/admin');
+	} else {
+		res.render('login', {});
+	}
+});
+
+router.post('/login', (req, res) => {
+	if (req.body.password == process.env.ADMIN_PWD) {
+		req.session.admin = true
+		res.redirect('/admin');
+	} else {
+		res.redirect('/admin/login');
+	}
+})
+
+router.use((req, res, next) => {
+	if (req.session.admin) {
+		next();
+	} else {
+		res.redirect('/admin/login');
+	}
+});
+
+router.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+module.exports = router;
