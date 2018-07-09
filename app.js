d27eea69f1bfc033b0349448921d38b010939f1c
bluephlavio <flavio.grandin@gmail.com>
2018-07-09 08:13:19 +0200

    handlebars template engine setup

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ require('dotenv')
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(session({
+	secret: 'secret',
+	resave: false,
+	saveUninitialized: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,13 +27,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(favicon(path.join(__dirname, 'client', 'build', 'favicon.png'), { maxAge: 0 }));
 app.use(logger('dev'));
 
+app.use('/admin', require('./routes/admin'));
 app.use('/api', require('./routes/api'));
 app.use('/test-flickr', require('./routes/test-flickr'));
 
-app.use(express.static(path.join(__dirname, 'client', 'build'), { maxAge: 0 }));
-app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+// app.use('/', express.static(path.join(__dirname, 'client', 'build'), { maxAge: 0 }));
+// app.get('/*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+// });
 
 
 module.exports = app;
