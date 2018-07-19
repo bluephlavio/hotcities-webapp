de8d513275b9df8abd46981e51489b5b3bb6fff7
bluephlavio <flavio.grandin@gmail.com>
2018-07-19 10:29:01 +0200

    express middlewares clean up

--- app.js
+++ app.js
@@ -4,10 +4,48 @@ require('dotenv')
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const LocalStrategy = require('passport-local')
+	.Strategy;
+const db = require('./db');
+
+passport.use(new LocalStrategy(
+	(username, password, done) => {
+		console.log('local strategy');
+		db.User.findOne({ username: username })
+			.then(user => {
+				if (user) {
+					if (user.password == password) {
+						console.log('passwords match');
+						return done(null, user);
+					} else {
+						console.log('password does not match');
+						return done(null, false);
+					}
+				} else {
+					console.log('user not found');
+					return done(null, false);
+				}
+			})
+			.catch(error => {
+				console.log('error');
+				return done(error);
+			});
+	}
+));
+
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	db.User.findById(id, function(err, user) {
+		done(err, user);
+	});
+});
 
 const app = express();
 
@@ -15,32 +53,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.use(session({
-	secret: 'secret',
-	resave: false,
-	saveUninitialized: true
-}));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(favicon(path.join(__dirname, 'client', 'build', 'favicon.png'), { maxAge: 0 }));
 app.use(logger('dev'));
-
+app.use(session({ secret: 'ahvaz', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(favicon(path.join(__dirname, 'client', 'build', 'favicon.png'), { maxAge: 0 }));
+app.use('/auth', require('./routes/auth'));
 app.use('/api', require('./routes/api'));
 app.use('/admin', require('./routes/admin'));
-
-if (process.env.NODE_ENV == 'development') {
-	app.use('/', express.static(path.join(__dirname, 'client', 'public')));
-	app.get('/*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
-	});
-} else {
-	app.use('/', express.static(path.join(__dirname, 'client', 'build')));
-	app.get('/*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-	});
-}
-
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')));
 
 module.exports = app;
