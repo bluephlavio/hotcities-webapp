9411c2150d1ed693da5137457f0c47e5c424bac3
bluephlavio <flavio.grandin@gmail.com>
2018-08-13 17:24:34 +0200

    auth-api interface implemented

---
+++ client/src/helpers/auth-api.js
@@ -0,0 +1,25 @@
+const login = (user) => {
+	return fetch('auth/login', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+			credentials: 'include',
+			body: JSON.stringify(user)
+		})
+		.then(res => {
+			return res.json();
+		})
+		.catch(err => console.log(err));
+}
+
+const logout = () => {
+	return fetch('auth/logout', {
+			method: 'GET',
+		})
+		.then(res => {
+			return res.json();
+		})
+		.catch(err => console.log(err));
+}
