fbec046905e38f49c85c2dd0b952c95cf7040fe8
bluephlavio <flavio.grandin@gmail.com>
2018-08-13 17:39:58 +0200

    auth helpers merged all together in one single file

--- client/src/helpers/auth.js
+++ client/src/helpers/auth.js
@@ -24,7 +24,29 @@ const logout = () => {
 		.catch(err => console.log(err));
 }
 
+const registerAuth = jwt => {
+	if (typeof window !== 'undefined') {
+		sessionStorage.setItem('jwt', JSON.stringify(jwt));
+	}
+}
+
+const unregisterAuth = () => {
+	if (typeof window !== 'undefined') {
+		sessionStorage.removeItem('jwt');
+	}
+}
+
+const readAuth = () => {
+	if (typeof window !== 'undefined') {
+		return sessionStorage.getItem('jwt');
+	}
+}
+
+
 export default {
 	login,
 	logout,
+	registerAuth,
+	unregisterAuth,
+	readAuth,
 }
