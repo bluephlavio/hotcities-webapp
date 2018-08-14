9a7f7199fb907c6fda3d4be7885a2c737b259ed1
bluephlavio <flavio.grandin@gmail.com>
2018-08-14 11:41:48 +0200

    auth helpers cleaned up and improved

--- client/src/helpers/auth.js
+++ client/src/helpers/auth.js
@@ -1,52 +1,45 @@
-const login = (user) => {
-	return fetch('auth/login', {
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json',
-			},
-			credentials: 'include',
-			body: JSON.stringify(user)
-		})
-		.then(res => {
-			return res.json();
-		})
-		.catch(err => console.log(err));
-}
+const login = user => fetch('auth/login', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  credentials: 'include',
+  body: JSON.stringify(user),
+})
+  .then(res => res.json())
+  .catch(err => console.log(err));
 
-const logout = () => {
-	return fetch('auth/logout', {
-			method: 'GET',
-		})
-		.then(res => {
-			return res.json();
-		})
-		.catch(err => console.log(err));
-}
+const logout = () => fetch('auth/logout', {
+  method: 'GET',
+})
+  .then(res => res.json())
+  .catch(err => console.log(err));
 
-const registerAuth = jwt => {
-	if (typeof window !== 'undefined') {
-		sessionStorage.setItem('jwt', JSON.stringify(jwt));
-	}
-}
+const registerAuth = (jwt) => {
+  if (typeof window !== 'undefined') {
+    sessionStorage.setItem('jwt', JSON.stringify(jwt));
+  }
+};
 
 const unregisterAuth = () => {
-	if (typeof window !== 'undefined') {
-		sessionStorage.removeItem('jwt');
-	}
-}
+  if (typeof window !== 'undefined') {
+    sessionStorage.removeItem('jwt');
+  }
+};
 
 const readAuth = () => {
-	if (typeof window !== 'undefined') {
-		return sessionStorage.getItem('jwt');
-	}
-}
+  if (typeof window !== 'undefined') {
+    return sessionStorage.getItem('jwt');
+  }
+  return false;
+};
 
 
 export default {
-	login,
-	logout,
-	registerAuth,
-	unregisterAuth,
-	readAuth,
-}
+  login,
+  logout,
+  registerAuth,
+  unregisterAuth,
+  readAuth,
+};
