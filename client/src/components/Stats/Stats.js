d3a8fe607e35b24d8205b73795e9827fbca30be4
bluephlavio <flavio.grandin@gmail.com>
2018-08-16 11:06:15 +0200

    stats route bug fixes

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
 import geojson from 'geojson';
 import _ from 'underscore';
-import fmt from '../../helpers/formatter';
+import formatter from '../../helpers/formatter';
 import Map from '../Map/Map';
 import InfoPanel from '../InfoPanel/InfoPanel';
 import './Stats.scss';
 
 
-const Item = props => (
-  <tr className="stats-item">
-    <th nowrap="nowrap" scope="row">{props.index}</th>
-    <td nowrap="nowrap">{fmt.names(props.item.name, props.item.localname)}</td>
-    <td nowrap="nowrap">{fmt.fracToPerc(props.item.recordFrac)}</td>
-    <td nowrap="nowrap">{fmt.temp(props.item.recordTemp)}</td>
-  </tr>
-);
+const Item = (props) => {
+  const { index, item } = props;
+  const {
+    formatNames,
+    formatTemp,
+    fracToPerc,
+  } = formatter;
+  return (
+    <tr className="stats-item">
+      <th nowrap="nowrap" scope="row">{index}</th>
+      <td nowrap="nowrap">{formatNames(item.name, item.localname)}</td>
+      <td nowrap="nowrap">{fracToPerc(item.recordFrac)}</td>
+      <td nowrap="nowrap">{formatTemp(item.recordTemp)}</td>
+    </tr>
+  );
+};
 
 class Ranking extends Component {
+  static rankByRecordFrac(data) {
+    return _.sortBy(data, item => -item.recordFrac);
+  }
+
+  static rankByRecordTemp(data) {
+    return _.sortBy(data, item => -item.recordTemp);
+  }
+
   constructor(props) {
     super(props);
     this.state = {
-      rankFn: this.rankByRecordTemp,
+      rankFn: Ranking.rankByRecordTemp,
       expanded: true,
     };
   }
@@ -33,19 +49,13 @@ class Ranking extends Component {
     };
   }
 
-  rankByRecordFrac(data) {
-    return _.sortBy(data, item => -item.recordFrac);
-  }
-
-  rankByRecordTemp(data) {
-    return _.sortBy(data, item => -item.recordTemp);
-  }
-
   rank() {
-    if (this.state.expanded) {
-      return this.state.rankFn(this.props.data);
+    const { expanded, rankFn } = this.state;
+    const { data } = this.props;
+    if (expanded) {
+      return rankFn(data);
     }
-    return _.first(this.state.rankFn(this.props.data), 5);
+    return _.first(rankFn(data), 5);
   }
 
   rows() {
@@ -56,6 +66,7 @@ class Ranking extends Component {
   }
 
   render() {
+    const { rankFn } = this.state;
     return (
       <div className="ranking">
         <table className="table-hover table-sm table-responsive d-table">
@@ -63,19 +74,19 @@ class Ranking extends Component {
             <tr>
               <th nowrap="nowrap" scope="col">#</th>
               <th nowrap="nowrap" scope="col">city</th>
-              <th nowrap="nowrap" scope="col" onClick={this.changeRankFn(this.rankByRecordFrac)}>
-                                record fraction
-                {this.state.rankFn === this.rankByRecordFrac
+              <th nowrap="nowrap" scope="col" onClick={this.changeRankFn(Ranking.rankByRecordFrac)}>
+                record fraction
+                {rankFn === Ranking.rankByRecordFrac
                   ? <span className="fas fa-circle" />
                   : <span className="far fa-circle" />
-                                }
+                }
               </th>
-              <th nowrap="nowrap" scope="col" onClick={this.changeRankFn(this.rankByRecordTemp)}>
-                                record temperature
-                {this.state.rankFn === this.rankByRecordTemp
+              <th nowrap="nowrap" scope="col" onClick={this.changeRankFn(Ranking.rankByRecordTemp)}>
+                record temperature
+                {rankFn === Ranking.rankByRecordTemp
                   ? <span className="fas fa-circle" />
                   : <span className="far fa-circle" />
-                                }
+                }
               </th>
             </tr>
           </thead>
@@ -94,6 +105,7 @@ class Stats extends Component {
     this.state = {
       isLoading: true,
     };
+    this.loadMapData = this.loadMapData.bind(this);
   }
 
   componentDidMount() {
@@ -116,21 +128,23 @@ class Stats extends Component {
   }
 
   caption() {
-    if (this.state.isLoading) {
+    const { isLoading } = this.state;
+    if (isLoading) {
       return 'Loading...';
     }
     return 'Stats';
   }
 
   loadMapData(map) {
-    const recordCities = _.map(this.state.data.recordCities, (entry) => {
-      const data = entry;
-      data.circleRadius = Math.sqrt(entry.recordFrac);
-      data.circleColor = entry.recordTemp;
-      return data;
+    const { data } = this.state;
+    const recordCities = _.map(data.recordCities, (entry) => {
+      const city = entry;
+      city.circleRadius = Math.sqrt(entry.recordFrac);
+      city.circleColor = entry.recordTemp;
+      return city;
     });
-    const recordTemps = _.map(this.state.data.recordCities, entry => entry.recordTemp);
-    const recordFracs = _.map(this.state.data.recordCities, entry => entry.recordFrac);
+    const recordTemps = _.map(data.recordCities, entry => entry.recordTemp);
+    const recordFracs = _.map(data.recordCities, entry => entry.recordFrac);
     return () => {
       map.addLayer({
         id: 'records',
@@ -161,27 +175,28 @@ class Stats extends Component {
   }
 
   render() {
+    const { isLoading, data } = this.state;
     return (
       <div className="stats">
         <div className="stats-view">
-          {!this.state.isLoading
-                        && (
-                        <Map
-                          mapstyle="mapbox://styles/bluephlavio/cjin553wo0vr92rrynvsksfuv"
-                          zoom={1}
-                          center={[0, 0]}
-                          load={this.loadMapData.bind(this)}
-                        />
-                        )
-                    }
+          {!isLoading && (
+            <Map
+              mapstyle="mapbox://styles/bluephlavio/cjin553wo0vr92rrynvsksfuv"
+              zoom={1}
+              center={[0, 0]}
+              load={this.loadMapData}
+            />
+          )
+          }
         </div>
         <InfoPanel
           title={this.caption()}
-          isLoading={this.state.isLoading}
+          isLoading={isLoading}
         >
-          {!this.state.isLoading
-                        && <Ranking data={this.state.data.recordCities} />
-                    }
+          {!isLoading && (
+            <Ranking data={data.recordCities} />
+          )
+          }
         </InfoPanel>
       </div>
     );
