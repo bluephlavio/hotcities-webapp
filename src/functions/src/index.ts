b0a914f40dc053df6124b850c32ec183e6021af0
bluephlavio <flavio.grandin@gmail.com>
2019-03-19 17:37:49 +0100

    package.json cleaned up

--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -2,13 +2,10 @@ import * as functions from 'firebase-functions';
 import * as next from 'next';
 import * as path from 'path';
 
-console.log(`${path.relative(process.cwd(), __dirname)}/next`);
-
-const dev = false; //process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({
   dev,
-  dir: __dirname,
-  conf: { distDir: `${path.relative(process.cwd(), __dirname)}/next` }
+  conf: { distDir: `${path.join(path.relative(process.cwd(), __dirname), '..', 'next')}` }
 });
 const handle = app.getRequestHandler();
 
