63b6a085b17e3674874accaf281d40b827076ce7
bluephlavio <flavio.grandin@gmail.com>
2019-04-17 15:43:13 +0200

    eslint and prettier integrated into project

--- components/Slideshow/index.js
+++ components/Slideshow/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   Carousel,
   CarouselItem,
@@ -13,18 +14,31 @@ const Attribution = ({ view }) => {
   return (
     <div className={style.attribution}>
       <a href={page} rel="noopener noreferrer" target="_blank">
-        { title || 'Untitled' }
+        {title || 'Untitled'}
       </a>
       {' by '}
       <a href={owner.page} rel="noopener noreferrer" target="_blank">
-        { owner.realname }
+        {owner.realname}
       </a>
       {', '}
       <a href={license.link} rel="noopener noreferrer" target="_blank">
-        { license.abbr }
+        {license.abbr}
       </a>
     </div>
   );
+};
+
+Attribution.propTypes = {
+  view: PropTypes.shape({
+    page: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    owner: PropTypes.shape({
+      realname: PropTypes.string.isRequired
+    }).isRequired,
+    license: PropTypes.shape({
+      abbr: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
 };
 
 class Slideshow extends Component {
@@ -47,18 +61,20 @@ class Slideshow extends Component {
   }
 
   next() {
+    const { activeIndex } = this.state;
     const { views } = this.props;
 
     if (this.animating) return;
-    const nextIndex = this.state.activeIndex == views.length - 1 ? 0 : this.state.activeIndex + 1;
+    const nextIndex = activeIndex === views.length - 1 ? 0 : activeIndex + 1;
     this.setState({ activeIndex: nextIndex });
   }
 
   previous() {
+    const { activeIndex } = this.state;
     const { views } = this.props;
 
     if (this.animating) return;
-    const nextIndex = this.state.activeIndex === 0 ? views.length - 1 : this.state.activeIndex - 1;
+    const nextIndex = activeIndex === 0 ? views.length - 1 : activeIndex - 1;
     this.setState({ activeIndex: nextIndex });
   }
 
@@ -72,15 +88,11 @@ class Slideshow extends Component {
     const { views } = this.props;
     const activeView = views[activeIndex];
 
-    const slides = views.map((view) => {
+    const slides = views.map(view => {
       return (
-        <CarouselItem
-          onExiting={this.onExiting}
-          onExited={this.onExited}
-          key={view.src}
-        >
-          <div className={style.view} style={{ backgroundImage: `url(${activeView.src})` }} />
-          <CarouselCaption captionText={activeView.title} captionHeader={activeView.title} />
+        <CarouselItem onExiting={this.onExiting} onExited={this.onExited} key={view.src}>
+          <div className={style.view} style={{ backgroundImage: `url(${view.src})` }} />
+          <CarouselCaption captionText={view.title} captionHeader={view.title} />
         </CarouselItem>
       );
     });
@@ -93,9 +105,17 @@ class Slideshow extends Component {
           next={this.next}
           previous={this.previous}
         >
-          <CarouselIndicators items={views} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+          <CarouselIndicators
+            items={views}
+            activeIndex={activeIndex}
+            onClickHandler={this.goToIndex}
+          />
           {slides}
-          <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+          <CarouselControl
+            direction="prev"
+            directionText="Previous"
+            onClickHandler={this.previous}
+          />
           <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
         </Carousel>
         <Attribution view={activeView} />
@@ -103,5 +123,13 @@ class Slideshow extends Component {
     );
   }
 }
+
+Slideshow.propTypes = {
+  views: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
 
 export default Slideshow;
