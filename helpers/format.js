46f3fc86ce61f5c370baa2ddff6d258ecd3de4ba
Flavio Grandin <flavio.grandin@gmail.com>
2019-06-28 19:51:20 +0200

    formatters refined

--- helpers/format.js
+++ helpers/format.js
@@ -1,10 +1,10 @@
 import formatcoords from 'formatcoords';
 
 export const formatNames = (name, localname) =>
-  `${name}${localname && localname !== name ? ` | ${localname}` : ''}`;
+  `${name}${localname && localname !== name ? ` / ${localname}` : ''}`;
 
-export const formatTemp = temp => `${temp} °C`;
+export const formatTemp = temp => `${Math.round(temp)} °C / ${Math.round(temp * 1.8 + 32)} °F`;
 
 export const formatCountry = (name, code) => `${name} (${code})`;
 
-export const formatCoords = coords => formatcoords(coords).format();
+export const formatCoords = coords => formatcoords(coords).format({ decimalPlaces: 0 });
