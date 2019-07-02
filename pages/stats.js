96b08b167a5808fa0dd8a0ea7505a6f5fec4cf96
Flavio Grandin <flavio.grandin@gmail.com>
2019-07-02 16:44:20 +0200

    stats page is ready for production

--- pages/stats.js
+++ pages/stats.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import dynamic from 'next/dynamic';
 import PropTypes from 'prop-types';
 import fetch from 'isomorphic-unfetch';
 import Panel from '../components/Panel';
-import Map from '../components/Map';
 import Ranking from '../components/Ranking';
+import Loading from '../components/Loading';
 import config from '../config';
+
+/* eslint-disable */
+const Map = dynamic(() => import('../components/Map'), {
+  ssr: false,
+  loading: Loading
+});
+/* eslint-enable */
 
 class Stats extends Component {
   static async getInitialProps({ pageProps }) {
