ca14fb6cbe3c0ce7427dbb2eec8a3dbe7b72f9eb
Flavio Grandin <flavio.grandin@gmail.com>
2019-07-05 20:27:55 +0200

    formatters revisited

--- helpers/format.js
+++ helpers/format.js
@@ -1,12 +1,20 @@
 import formatcoords from 'formatcoords';
 
-export const formatNames = (name, localname) =>
+export const formatNames = ({ name, localname }) =>
   `${name}${localname && localname !== name ? ` / ${localname}` : ''}`;
 
-export const formatTemp = temp => `${Math.round(temp)} °C / ${Math.round(temp * 1.8 + 32)} °F`;
+export const formatTemp = temp =>
+  `${Math.round(temp)} °C / ${Math.round(temp * 1.8 + 32)} °F`;
 
-export const formatCountry = (name, code) => `${name} (${code})`;
+export const formatCountry = ({ countryname: name, countrycode: code }) =>
+  `${name} (${code})`;
 
-export const formatCoords = coords => formatcoords(coords).format({ decimalPlaces: 0 });
+export const formatCoords = ({ lng, lat }) =>
+  formatcoords({ lng, lat }).format({ decimalPlaces: 0 });
 
 export const formatFracAsPerc = frac => `${(frac * 100).toFixed(0)} %`;
+
+export const formatRank = ({ rank, score }) =>
+  `Ranked ${rank}th with a score of ${score.toFixed(2)}`;
+
+export const formatPopulation = population => `${population} inhab.`;
