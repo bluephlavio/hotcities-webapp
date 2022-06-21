b13b4096e95e5ea7c9674b36da79bea752adae3c
Flavio Grandin <flavio.grandin@gmail.com>
2022-06-21 14:46:52 +0200

    feat(stats): stats title and help added

--- components/StatsTab/StatsTab.js
+++ components/StatsTab/StatsTab.js
@@ -4,7 +4,8 @@ import styles from './StatsTab.module.scss';
 
 const StatsTab = () => (
     <div className={styles.container}>
-      <h1>All time Stats</h1>
+      <h1>All time city ranking</h1>
+      <p>Change the sorting key by clicking on the headers</p>
       <Ranking />
     </div>
   );
